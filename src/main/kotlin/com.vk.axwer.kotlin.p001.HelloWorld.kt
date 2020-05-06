9068e5c13eacba5a3eee53113c76ebd29adad080
Marat Gabitov <marat.kungur@gmail.com>
2020-05-06 20:23:46 +0500

    Kotlin. Starting.

---
+++ src/main/kotlin/com.vk.axwer.kotlin.p001.HelloWorld.kt
@@ -0,0 +1,15 @@
+// Класс данных
+data class Person(
+        val name: String,
+        val age: Int? = null    // Тип, допускающий значение null (Int?); значение параметра по умолчанию
+)
+
+// Функция верхнего уровня
+fun main(args: Array<String>) {
+    val persons = listOf(
+            Person("Alice"),
+            Person("Bob", age = 29)       // Именованный аргумент
+    )
+    val oldest = persons.maxBy { it.age ?: 0 }  // Лямбда-выражение; оператор "Элвис"
+    println("The oldest is: $oldest")           // Строка-шаблон
+}
